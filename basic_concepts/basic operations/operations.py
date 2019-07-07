1ee8daad2dce0350d2497d32dc714fd5a4500563
kareem sameer <k.harazeen@gmail.com>
2019-07-07 14:01:36 +0300

    simple operations

---
+++ basic_concepts/basic operations/operations.py
@@ -0,0 +1,28 @@
+# we can write operation in python console directly, like: >>> 2-6
+
+print(2 + 6)
+
+print(8 / 4) # will results float number
+
+print(8 / 5) # will results float number 
+
+# summation and multiplication of 2 integers will result integers.
+print(2 + 5)
+print(2 * 8)
+
+# division will always results in float.
+print(3/4)
+
+
+# Computers can't store floats perfectly accurately, in the same way 
+# that we can't write down the complete decimal expansion of 
+# 1/3 (0.3333333333333333...). because it often leads to infuriating bugs!
+print(1 / 3)
+
+
+
+# we can also convert integers to floats, 
+print(float(5))
+
+# or floats to integers 
+print(int(5.6))
--- basic_concepts/hello/hello.py
+++ basic_concepts/hello/hello.py
@@ -1,1 +1,3 @@
 print("hello world!")
+
+print("here is how we print in PHP: echo \"Hello World\"")
